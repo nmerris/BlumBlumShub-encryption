a9a68cc997711db82309c0d05b7018c7f3b92db1
Nathan Merris <joorge.jetson@gmail.com>
2017-10-10 13:53:23 -0400

    cleaned up some more, seems to be decoding and encoding ok.. need to do more testing

--- src/com/nmerris/Main.java
+++ src/com/nmerris/Main.java
@@ -1,5 +1,6 @@
 package com.nmerris;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -20,7 +21,10 @@ public class Main {
 
         // the initialValue entered by user, need BigInteger again because could overflow Long if user
         // types in too large a hex number, for example FFFFFFFF will overflow Long after it is squared
-        BigInteger initialValue = BigInteger.ZERO;
+        BigInteger initialValue;
+
+        // true if user wants to encode, false if decode
+        boolean encode;
 
         // get user input
         System.out.println("Enter data: ");
@@ -31,54 +35,61 @@ public class Main {
         scanner.nextLine(); // consume the dangling \n
 
 
-        // get the initial hex value, do some basic validation
-        boolean invalidInput;
-        do {
-            invalidInput = false;
-            System.out.println("Enter initial value in hex: ");
-            String initialValueString = scanner.nextLine();
-            try {
-                // convert the entered String to a BigInteger, input will always be in hexadecimal
-                initialValue = new BigInteger(initialValueString, 16);
-            } catch (NumberFormatException e) {
-                // user did not enter a valid hex value
-                System.out.println("Please enter a valid hexidecimal value...");
-                invalidInput = true;
+        // get the initial value as a hex, save it as a BigInteger
+        System.out.println("Enter initial value in hex: ");
+        String initialValueString = scanner.nextLine();
+        initialValue = new BigInteger(initialValueString, 16);
+
+        // holds each byte of the input: could be either data to encode or decode
+        byte[] inputByteArray = new byte[dataLength];
+
+        System.out.println("Would you like to ENCODE or DECODE? (E/D)");
+        // set boolean depending on if user wants to encode or decode
+        encode = scanner.nextLine().equalsIgnoreCase("E");
+
+        if (encode) {
+            // user selected ENCODE
+
+            // this challenge assumes 1 byte per character, so I'm chopping off the unused first byte
+            // this happens automatically when converting data to an array of Bytes
+            // by default, the bytes are UTF-8 encoded
+            inputByteArray = data.getBytes();
+
+        } else {
+            // user selected DECODE
+
+            // for decoding, we start with 2 hex digits per char, so need to combine consecutive groups
+            // to get a starting array of bytes
+            for (int i = 0; i < dataLength * 2; i += 2) {
+                // get 2 consecutive bytes each time, so: 0,1 then 2,3 then 4,5 etc
+                // substrings upper index is not inclusive, thus i + 2
+                int tempByte = Integer.valueOf(data.substring(i, i + 2), 16);
+                // put the newly created 'combo' byte in the input byte array
+                inputByteArray[i / 2] = (byte) (tempByte);
             }
-        } while (invalidInput);
+        }
 
 
-        // NOTE: in Java, all chars are stored as 2 bytes (4 hex digits)
-        // this challenge assumes 1 byte per character, so I'm chopping off the unused first byte
-        // this happens automatically when converting data to an array of Bytes
-        byte[] inputByteArray = data.getBytes();
+        // holds the encoded output String
+        StringBuilder encodedOutputString = new StringBuilder(dataLength);
 
-
-        // holds the encrypted output String
-        StringBuilder output = new StringBuilder(dataLength);
+        // holds the decoded output bytes, these will be converted to a String when displaying to console
+        byte[] decodedOutputByteArray = new byte[dataLength];
 
         // calculate the initial value: value = (value * value) mod M
         BigInteger value = initialValue.pow(2).mod(M);
 
-
-//        value = value.mod(M);
-//        BigInteger value = BigInteger.valueOf(initialValue * initialValue).mod(M);
-
-//        System.out.println(String.format("length of value & lowestByteMask: %d", value.and(lowestByteMask).toByteArray().length));
-
-//        byte lowByte = value.toByteArray()[1];
-
-//        System.out.println(String.format("initial value squared: %20X", initialValue.pow(2)));
-//        System.out.println(String.format("initial value after squaring and mod: %10X", value));
-//        System.out.println(String.format("inputByteArray[0]: %10X", inputByteArray[0]));
-//        System.out.println("initial value.toByteArray().length: " + value.toByteArray().length);
-//        System.out.println("initial value.and(lowestByteMask).toByteArray().length: " + value.and(lowestByteMask).toByteArray().length);
-
-        // get the lowest byte, keep in mind value may have varying numbers of bytes
+        // get the lowest byte: mask out everything except the lowest byte, convert to byte array,
+        // then grabe the lowest byte of the array
+        // NOTE: value may have varying number of bytes, so need to get it's length and subtract 1 to get the lowest byte
         byte lowByte = value.and(lowestByteMask).toByteArray()[value.and(lowestByteMask).toByteArray().length - 1];
 
-        // XOR lowByte with the first byte in inputByteArray, cast to a byte to chop off the unwanted preceeding digits
-        output.append(String.format("%02X ", (byte) (lowByte ^ inputByteArray[0])));
+        // XOR lowByte with the first byte in inputByteArray, cast to a byte to chop off the unwanted preceding digits
+        if (encode) {
+            encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ inputByteArray[0])));
+        } else {
+            decodedOutputByteArray[0] = (byte) (lowByte ^ inputByteArray[0]);
+        }
 
         // loop through the rest of the input, one byte at a time
         for (int i = 1; i < dataLength; i++) {
@@ -89,32 +100,32 @@ public class Main {
             // get the lowest byte, this is the current key
             lowByte = value.and(lowestByteMask).toByteArray()[value.and(lowestByteMask).toByteArray().length - 1];
 
-            // append the encrypted byte to the output String
-            output.append(String.format("%02X ", (byte) (lowByte ^ inputByteArray[i])));
+            if (encode) {
+                // append the encoded byte to the output String, drop all preceding bytes by casting to byte
+                encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ inputByteArray[i])));
+            } else {
+                // add this byte to the decoded array of bytes that will represent our decoded output
+                decodedOutputByteArray[i] = (byte) (lowByte ^ inputByteArray[i]);
+            }
 
         }
 
 
-
-        System.out.println("FINAL OUTPUT ENCRYPTED: " + output);
-
-
-    }
-
-
-
-
-
-    // CREDIT for this method: https://stackoverflow.com/questions/923863/converting-a-string-to-hexadecimal-in-java, Bogdan Calmac
-    static String stringToHex(String string) {
-        StringBuilder buf = new StringBuilder(200);
-        for (char ch: string.toCharArray()) {
-            if (buf.length() > 0)
-                buf.append(' ');
-            buf.append(String.format("%04x", (int) ch));
+        // display the output depending on if user selected 'encode' or 'decode'
+        if (encode) {
+            // display the encoded output
+            System.out.println("OUTPUT ENCODED: " + encodedOutputString);
+        } else {
+            try {
+                // display the decoded output, create a new String from the array of bytes using UTF8 encoding
+                System.out.println("OUTPUT DECODED: " + new String(decodedOutputByteArray, "UTF8"));
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
         }
-        return buf.toString();
-    }
+
+
+    } // main
 
 
 }
