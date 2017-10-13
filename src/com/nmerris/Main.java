42800ef09e6db45f5901e8f978699f4179cb1323
Nathan Merris <joorge.jetson@gmail.com>
2017-10-13 10:55:55 -0400

    more clean up, extracted BBS method from main to match signature specified, changed input to allow unsigned ints up to full 2^32 - 1 range

--- src/com/nmerris/Main.java
+++ src/com/nmerris/Main.java
@@ -11,6 +11,32 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        // get user input
+        System.out.println("Enter data: ");
+        String dataString = scanner.nextLine();
+
+        System.out.println("Enter data length: ");
+        int dataLength = scanner.nextInt();
+        scanner.nextLine(); // consume the dangling \n
+
+        // get the initial value as a hex
+        System.out.println("Enter a positive initial value <= 99 999 999 in hex: ");
+        String initialValueString = scanner.nextLine();
+
+        try {
+            // convert initialValueString to an int, user can enter an unsigned int here
+            // so there are 2^32 = 0x99999999 possible initialValues, and must be a positive number
+            int initialValue = Integer.parseUnsignedInt(initialValueString, 16);
+            blumBlumShubify(dataString , dataLength, initialValue);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid initial value");
+        }
+
+    } // main
+
+
+    private static void blumBlumShubify(String dataString, int dataLength, int initialValue) {
+
         // squaring the initialValue for BBS PRNG can result in very large numbers
         // since we don't know how large initialValue may be, best to use BigInteger here
         // or we may overflow and get bogus results
@@ -19,56 +45,34 @@ public class Main {
         // used to select the lowest byte of a number
         final BigInteger lowestByteMask = BigInteger.valueOf(0xFFL);
 
-        // the initialValue entered by user, need BigInteger again because could overflow Long if user
-        // types in too large a hex number, for example FFFFFFFF will overflow Long after it is squared
-        BigInteger initialValue;
-
-        // true if user wants to encode, false if decode
-        boolean encode;
-
-        // get user input
-        System.out.println("Enter data: ");
-        String data = scanner.nextLine();
-
-        System.out.println("Enter data length: ");
-        int dataLength = scanner.nextInt();
-        scanner.nextLine(); // consume the dangling \n
-
-
-        // get the initial value as a hex, save it as a BigInteger
-        System.out.println("Enter initial value in hex: ");
-        String initialValueString = scanner.nextLine();
-        initialValue = new BigInteger(initialValueString, 16);
+        // if the number of chars entered as data is same as data length entered by user, must be encoding
+        // otherwise, for the purposes of this simple program, we'll assume user wants to decode
+        boolean encode = dataLength == dataString.length();
 
         // holds each byte of the input: could be either data to encode or decode
-        byte[] inputByteArray = new byte[dataLength];
-
-        System.out.println("Would you like to ENCODE or DECODE? (E/D)");
-        // set boolean depending on if user wants to encode or decode
-        encode = scanner.nextLine().equalsIgnoreCase("E");
+        byte[] data = new byte[dataLength];
 
         if (encode) {
-            // user selected ENCODE
-
             // this challenge assumes 1 byte per character, so I'm chopping off the unused first byte
             // this happens automatically when converting data to an array of Bytes
             // by default, the bytes are UTF-8 encoded
-            inputByteArray = data.getBytes();
-
+            data = dataString.getBytes();
         } else {
-            // user selected DECODE
-
             // for decoding, we start with 2 hex digits per char, so need to combine consecutive groups
             // to get a starting array of bytes
             for (int i = 0; i < dataLength * 2; i += 2) {
                 // get 2 consecutive bytes each time, so: 0,1 then 2,3 then 4,5 etc
                 // substrings upper index is not inclusive, thus i + 2
-                int tempByte = Integer.valueOf(data.substring(i, i + 2), 16);
+                int tempByte = Integer.valueOf(dataString.substring(i, i + 2), 16);
                 // put the newly created 'combo' byte in the input byte array
-                inputByteArray[i / 2] = (byte) (tempByte);
+                data[i / 2] = (byte) (tempByte);
             }
         }
-
+        
+        
+        
+        
+        
 
         // holds the encoded output String
         StringBuilder encodedOutputString = new StringBuilder(dataLength);
@@ -77,18 +81,18 @@ public class Main {
         byte[] decodedOutputByteArray = new byte[dataLength];
 
         // calculate the initial value: value = (value * value) mod M
-        BigInteger value = initialValue.pow(2).mod(M);
+        BigInteger value = BigInteger.valueOf(initialValue).pow(2).mod(M);
 
         // get the lowest byte: mask out everything except the lowest byte, convert to byte array,
-        // then grabe the lowest byte of the array
+        // then grab the lowest byte of the array
         // NOTE: value may have varying number of bytes, so need to get it's length and subtract 1 to get the lowest byte
         byte lowByte = value.and(lowestByteMask).toByteArray()[value.and(lowestByteMask).toByteArray().length - 1];
 
-        // XOR lowByte with the first byte in inputByteArray, cast to a byte to chop off the unwanted preceding digits
+        // XOR lowByte with the first byte in data, cast to a byte to chop off the unwanted preceding digits
         if (encode) {
-            encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ inputByteArray[0])));
+            encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ data[0])));
         } else {
-            decodedOutputByteArray[0] = (byte) (lowByte ^ inputByteArray[0]);
+            decodedOutputByteArray[0] = (byte) (lowByte ^ data[0]);
         }
 
         // loop through the rest of the input, one byte at a time
@@ -102,18 +106,15 @@ public class Main {
 
             if (encode) {
                 // append the encoded byte to the output String, drop all preceding bytes by casting to byte
-                encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ inputByteArray[i])));
+                encodedOutputString.append(String.format("%02X", (byte) (lowByte ^ data[i])));
             } else {
                 // add this byte to the decoded array of bytes that will represent our decoded output
-                decodedOutputByteArray[i] = (byte) (lowByte ^ inputByteArray[i]);
+                decodedOutputByteArray[i] = (byte) (lowByte ^ data[i]);
             }
-
         }
 
-
-        // display the output depending on if user selected 'encode' or 'decode'
+        // display the output depending on if we are encoding or decoding
         if (encode) {
-            // display the encoded output
             System.out.println("OUTPUT ENCODED: " + encodedOutputString);
         } else {
             try {
@@ -125,7 +126,13 @@ public class Main {
         }
 
 
-    } // main
+
+
+    } // blumBlumShubify
+
+
+
+
 
 
 }
